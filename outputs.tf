6c9b9142a8095c8f25298c5e42e8d6fb846db736
Christo De Lange <christo@quicknode.io>
2022-06-24 18:15:01 -0400

    vcn_id+vcn_cidr

--- outputs.tf
+++ outputs.tf
@@ -28,6 +28,14 @@ output "KubeConfig" {
   value = data.oci_containerengine_cluster_kube_config.KubeConfig.content
 }
 
+output "vcn_id" {
+  value = oci_core_vcn.oke_vcn.id
+}
+
+output "vcn_cidr" {
+  value = oci_core_vcn.oke_vcn.cidr_block
+}
+
 output "lb_subnet_id" {
   value = oci_core_subnet.oke_lb_subnet.id
 }
